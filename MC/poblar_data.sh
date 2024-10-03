80232d78378897933a6309865fa5fe9df1cf8e07
Mikel_Bracamonte <mikel.bracamonte@utec.edu.pe>
2024-10-03 02:39:28 -0500

    puse los jsons para mongo y un script para importarlos. estoy intentando hacer un script que se conecte a las bases de datos y importe los csvs y todo. a ver si funciona.

---
+++ MC/poblar_data.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+CURRENT_DIR=$(dirname "$0")
+
+# Archivo SQL
+SQL_FILE="$CURRENT_DIR/importar_datos.sql"
+
+# Vaciado de las tablas en PostgreSQL
+echo "Vaciando las tablas en la base de datos PostgreSQL..."
+PGPASSWORD=$POSTGRES_PASSWORD psql -h localhost -U $POSTGRES_USER -d $POSTGRES_DB -p $POSTGRES_PORT -c "TRUNCATE TABLE cliente, persona, tienda, cuenta RESTART IDENTITY;"
+
+# Reemplazar la ruta del archivo SQL
+echo "Reemplazando rutas en el archivo SQL..."
+sed -i "s|/ruta/|$CURRENT_DIR/|g" $SQL_FILE
+
+# Importar los datos utilizando el archivo SQL
+echo "Importando los datos desde los CSVs..."
+PGPASSWORD=$POSTGRES_PASSWORD psql -h localhost -U $POSTGRES_USER -d $POSTGRES_DB -p $POSTGRES_PORT -f $SQL_FILE
+
+echo "Se pobló la base de datos."
