964fd9615b17f0eabe0eb1bf1c57d80f08c31275
Mikel_Bracamonte <mikel.bracamonte@utec.edu.pe>
2024-10-03 03:16:01 -0500

    arreglando el .sql

--- MC/insertar_data.sql
+++ MC/insertar_data.sql
@@ -1,21 +1,7 @@
---reemplazar /ruta/ por la ruta hasta el csv
+\COPY cliente(id, nombre) FROM '/ruta/clientes.csv' DELIMITER ',' CSV HEADER;
 
-COPY cliente(id, nombre)
-FROM '/ruta/clientes.csv'
-DELIMITER ','
-CSV HEADER;
+\COPY persona(cliente_id, telefono) FROM '/ruta/personas.csv' DELIMITER ',' CSV HEADER;
 
-COPY persona(cliente_id, telefono)
-FROM '/ruta/personas.csv'
-DELIMITER ','
-CSV HEADER;
+\COPY tienda(cliente_id, correo) FROM '/ruta/tiendas.csv' DELIMITER ',' CSV HEADER;
 
-COPY tienda(cliente_id, correo)
-FROM '/ruta/tiendas.csv'
-DELIMITER ','
-CSV HEADER;
-
-COPY cuenta(id, cliente_id, saldo)
-FROM '/ruta/cuentas.csv'
-DELIMITER ','
-CSV HEADER;
+\COPY cuenta(id, cliente_id, saldo) FROM '/ruta/cuentas.csv' DELIMITER ',' CSV HEADER;
