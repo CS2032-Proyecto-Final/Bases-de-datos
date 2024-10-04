990164a0ad02595bcb1ce5dcc6ae94e26fcd211d
EduardoAragon11 <eduardo.aragon@utec.edu.pe>
2024-10-04 09:11:26 -0500

    host

--- MM/poblar_data.sh
+++ MM/poblar_data.sh
@@ -11,6 +11,6 @@ SQL_FILE="$CURRENT_DIR/insertar_data.sql"
 
 # Importar los datos utilizando el archivo SQL
 echo "Importando los datos desde los CSVs..."
-mysql --local-infile=1 -h 127.0.0.1 -u root --password="${MYSQL_ROOT_PASSWORD}" -P $MYSQL_PORT $MYSQL_DATABASE < $SQL_FILE
+mysql --local-infile=1 -h $HOST -u root --password="${MYSQL_ROOT_PASSWORD}" -P $MYSQL_PORT $MYSQL_DATABASE < $SQL_FILE
 
 echo "Se pobló la base de datos."
--- MP/poblar_data.sh
+++ MP/poblar_data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mongoimport --host localhost --port 8002 --db promociones_db --collection productos --file products.json --jsonArray --drop
-mongoimport --host localhost --port 8002 --db promociones_db --collection promociones --file promociones.json --jsonArray --drop
+mongoimport --host $HOST --port 8002 --db promociones_db --collection productos --file products.json --jsonArray --drop
+mongoimport --host $HOST --port 8002 --db promociones_db --collection promociones --file promociones.json --jsonArray --drop
