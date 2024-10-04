a0edcb69ebd32aa41397fd8772a304d23f1c93e1
EduardoAragon11 <eduardo.aragon@utec.edu.pe>
2024-10-04 09:19:51 -0500

    atras

--- MC/poblar_data.sh
+++ MC/poblar_data.sh
@@ -11,6 +11,6 @@ SQL_FILE="$CURRENT_DIR/insertar_data.sql"
 
 # Importar los datos utilizando el archivo SQL
 echo "Importando los datos desde los CSVs..."
-PGPASSWORD=$POSTGRES_PASSWORD psql -h $HOST -U $POSTGRES_USER -d $POSTGRES_DB -p $POSTGRES_PORT -f $SQL_FILE
+PGPASSWORD=$POSTGRES_PASSWORD psql -h localhost -U $POSTGRES_USER -d $POSTGRES_DB -p $POSTGRES_PORT -f $SQL_FILE
 
 echo "Se pobló la base de datos."
