b2f290422ecb38958d6e0f1389b46745354c88eb
Mikel_Bracamonte <mikel.bracamonte@utec.edu.pe>
2024-10-06 14:10:37 -0500

    las secuencias de las ids para el microservicio de clientes ahora empiezan desde el ultimo que se creo con los csvs, asi que ya no deberia explotar cuando crees un nuevo cliente

--- MC/insertar_data.sql
+++ MC/insertar_data.sql
@@ -7,3 +7,8 @@ TRUNCATE TABLE cliente, persona, tienda, cuenta RESTART IDENTITY;
 \COPY tienda(cliente_id, correo) FROM './tiendas.csv' DELIMITER ',' CSV HEADER;
 
 \COPY cuenta(id, cliente_id, saldo) FROM './cuentas.csv' DELIMITER ',' CSV HEADER;
+
+SELECT setval('cliente_id_seq', 1021);
+SELECT setval('cuenta_id_seq', 1021);
+SELECT setval('persona_id_seq', 1001);
+SELECT setval('tienda_id_seq', 21);
